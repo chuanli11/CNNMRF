1502a3e25e16107455b21dd3ffbe4c5986df1d18
chuanli <cl.chuanli@gmail.com>
2016-01-21 18:14:42 +0100

    example update

--- run_trans.lua
+++ run_trans.lua
@@ -22,8 +22,11 @@ transfer_CNNMRF_wrapper = require 'transfer_CNNMRF_wrapper'
 -- mrf_num_scale: To matching objects of different scales, we use a number of scaled copies of the style image. The total number of scaling is "2 * mrf_num_scale + 1"
 -- content_weight: The weight for content constraint. Increasing this value will make the result more content faithful. Decreasing the value will make the method more style faithful. Notice this value should be increase (for example, doubled) if layer 12 is included for MRF constraint,  
 local list_params = { 
-                     {'0', '0', 'image', {400, 400, 400, 200}, {21}, {3}, 3, 3, 2e0},
-                     {'1', '1', 'image', {400, 400, 400, 200}, {21}, {3}, 3, 3, 2e0},
+                    {'0', '0', 'image', {400, 400, 400, 200}, {21}, {3}, 3, 3, 2e0},
+                    {'1', '1', 'image', {400, 400, 400, 200}, {21}, {3}, 3, 3, 2e0},                     
+					{'potrait1', 'picasso', 'image', {400, 400, 400, 200}, {12, 21}, {3, 3}, 1, 1, 1e1} -- interpolation: balanced
+                    -- {'potrait1', 'picasso', 'image', {400, 400, 400, 200}, {12, 21}, {3, 3}, 1, 1, 2e1} -- interpolation: strong content
+                    -- {'potrait1', 'picasso', 'image', {400, 400, 400, 200}, {12, 21}, {3, 3}, 1, 1, 7e0} -- interpolation: more style
                     }    
 
 for i_test = 1, #list_params do
